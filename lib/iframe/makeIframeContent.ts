fb5d8f88a62679891b25f2912893b4f77cb8d708
Hori Yuma <hokkey@mac.com>
2017-10-09 20:11:14 +0900

    Add a bundled distribution

--- lib/iframe/makeIframeContent.ts
+++ lib/iframe/makeIframeContent.ts
@@ -1,18 +1,14 @@
 import {QiitaWidgetParam} from "../interface";
 
-import * as CSS from "lib/style/iframe.scss";
-
 // Included via raw-loader:
+import * as CSS from "lib/style/iframe.scss";
 import * as HTML from "lib/iframe/view/basic.html";
-import * as LIB from "dist/lib.js";
+import * as LIB from "dist/lib.bundled.js";
 
 export function makeIframeContent(data: QiitaWidgetParam, id: number): string {
   return `
 <style>${CSS}</style>
 <div id="widget">${HTML}</div>
-<script src="https://unpkg.com/axios@0.16.2/dist/axios.min.js"></script>
-<script src="https://unpkg.com/localforage@1.5.1/dist/localforage.min.js"></script>
-<script src="https://unpkg.com/axios-cache-adapter@1.5.2/dist/cache.bundled.min.js"></script>
 <script>
   ${LIB}
   (function() {
@@ -32,4 +28,4 @@ export function makeIframeContent(data: QiitaWidgetParam, id: number): string {
     });
   })();
 </script>`;
-  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,17 @@ const path = require('path');
 const webpack = require('webpack');
 
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
 const DEBUG = !process.argv.includes('--env.production');
+const banner = {
+  lib: 'qiita-widget-js | https://media-massage.net/qiita-widget-js | MIT License',
+  bundled: `qiita-widget-js | https://media-massage.net/qiita-widget-js | MIT License'
+This software includes localforage that is distributed in the Apache License 2.0
+
+Acknowledgements:
+axios | https://www.npmjs.com/package/axios | MIT License
+localforage | https://www.npmjs.com/package/localforage | Apache License 2.0
+axios-cache-adapter | https://www.npmjs.com/package/axios-cache-adapter | MIT License`
+};
 
 const jsPlugins = [
   new webpack.optimize.UglifyJsPlugin({
@@ -11,10 +20,7 @@ const jsPlugins = [
     compress: {screw_ie8: true, warnings: false}
   }),
   new webpack.optimize.OccurrenceOrderPlugin(),
-  new webpack.optimize.AggressiveMergingPlugin(),
-  new webpack.BannerPlugin(
-    'qiita-widget-js | MIT License | https://media-massage.net/qiita-widget-js/'
-  )
+  new webpack.optimize.AggressiveMergingPlugin()
 ];
 
 module.exports = [
@@ -51,7 +57,45 @@ module.exports = [
       modules: ['node_modules', path.resolve(__dirname, '')]
     },
 
-    plugins: jsPlugins,
+    plugins: jsPlugins.concat([
+      new webpack.BannerPlugin(banner.lib)
+    ]),
+    devtool: 'source-map',
+
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    }
+  },
+
+  // Library Build (bundled)
+  {
+    entry: {
+      'lib.bundled': "./lib/QiitaWidget.ts",
+    },
+
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+      library: 'QiitaWidget',
+      libraryTarget: 'umd',
+      libraryExport: 'default',
+      umdNamedDefine: true
+    },
+
+    resolve: {
+      extensions: ['.ts', '.json', '.js'],
+      modules: ['node_modules', path.resolve(__dirname, '')]
+    },
+
+    plugins: jsPlugins.concat([
+      new webpack.BannerPlugin(banner.bundled)
+    ]),
     devtool: 'source-map',
 
     module: {
@@ -126,7 +170,9 @@ module.exports = [
       modules: ['node_modules', path.resolve(__dirname, '')]
     },
 
-    plugins: jsPlugins,
+    plugins: jsPlugins.concat([
+      new webpack.BannerPlugin(banner.bundled)
+    ]),
     devtool: false,
 
     module: {
@@ -137,7 +183,7 @@ module.exports = [
           exclude: /node_modules/
         },
         {
-          test: /lib.js$/,
+          test: /lib.bundled.js$/,
           use: 'raw-loader',
           exclude: /node_modules/
         },
