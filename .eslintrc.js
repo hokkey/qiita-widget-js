e19e9cd29fe151ecc165563851ef18669098c9c5
Yuma Hori <hokkey@users.noreply.github.com>
2021-01-31 04:25:05 +0000

    Replace the linter rules and tsconfig to validate strictly

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2019,
+    // eslint-disable-next-line no-undef
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.eslint.json'],
+  },
+  ignorePatterns: ['*.d.ts'],
+  plugins: ['jest', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+}
